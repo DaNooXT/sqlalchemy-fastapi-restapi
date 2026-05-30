b7e4a809fdaf06baac36763914263c6c39ac339c
DaNooXT <raulkbauer2009@gmail.com>
2026-05-30 18:21:56 -0300

    feat: correction on the functions login, authenticate_user and create_token

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
 from datetime import datetime, timedelta, timezone
 from jose import jwt, JWTError
 
@@ -15,15 +16,16 @@ from app.core.dependencies import create_session
 auth_route = APIRouter(prefix="/auth", tags=["auth"])
 
 
-def create_token(dados, tempo_expiracao = timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)):
+def create_token(dados, token_type: str, tempo_expiracao = timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)):
 
     expiracao = datetime.now(timezone.utc) + tempo_expiracao
 
-    if tempo_expiracao <= 0 or tempo_expiracao > 100:
+    if tempo_expiracao.total_seconds() <= 0:
         raise ValueError("tempo invalido")
 
     dados_token = {
         "sub": str(dados),
+        "token_type": token_type,
         "exp": expiracao
     }
 
@@ -32,18 +34,19 @@ def create_token(dados, tempo_expiracao = timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
         SECRET_KEY,
         algorithm=ALGORITHM
     )
-
+    
     return token
 
-def authenticate_user (dados, session = Depends(create_session)):
+
+def authenticate_user (dados, session: Session):
     
     usuario = session.query(users).filter(users.email == dados.email).first()
 
     if not usuario:
-        raise HTTPException(status_code=400, detail="Usuario nao existe")
+        raise HTTPException(status_code=400, detail="Email ou senha invalidos")
     
     elif not bcrypt_context.verify(dados.senha, usuario.senha):
-        raise HTTPException(status_code=400, detail="Senha incorreta")
+        raise HTTPException(status_code=400, detail="Email ou senha invalidos")
     
     return usuario  
 
@@ -56,8 +59,9 @@ async def home():
     """
     return {"mensagem": "Rota acessada com sucesso"}
 
+
 @auth_route.post("/create_user")
-async def create_user(usuario: UserSchema, session = Depends(create_session)):
+async def create_user(usuario: UserSchema, session: Session = Depends(create_session)):
 
     novo_existente = session.query(users).filter(users.email == usuario.email).first()
 
@@ -81,7 +85,7 @@ async def create_user(usuario: UserSchema, session = Depends(create_session)):
     
 
 @auth_route.get("/get_user/{usuario_id}")
-async def get_user(usuario_id: int, session = Depends(create_session)):
+async def get_user(usuario_id: int, session: Session = Depends(create_session)):
 
     pergar_usuario = session.query(users).filter(users.id == usuario_id).first()
 
@@ -101,12 +105,12 @@ async def list_user(session = Depends(create_session)):
 
 
 @auth_route.post("/login")
-async def login (dados: UserEschemaLogin, session = Depends(create_session)):
+async def login (dados: UserEschemaLogin, session: Session = Depends(create_session)):
 
-    usuario = authenticate_user(dados)
-
-    access_token = create_token(usuario.id)
-    refresh_token = create_token(usuario.id, tempo_expiracao = timedelta(days=7))
+    usuario = authenticate_user(dados, session)
+    
+    access_token = create_token(usuario.id, token_type = "access")
+    refresh_token = create_token(usuario.id, token_type = "refresh", tempo_expiracao = timedelta(days=7))
 
     return {
         "access_token": access_token,
